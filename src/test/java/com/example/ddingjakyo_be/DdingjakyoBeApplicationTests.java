ad6d5781abd8b9bd0ceed6cf65573ee414c8256b
koseonje <kosunje1455@navet.com>
2023-11-06 01:22:03 +0900

    chore:빌드

---
+++ src/test/java/com/example/ddingjakyo_be/DdingjakyoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ddingjakyo_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdingjakyoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
