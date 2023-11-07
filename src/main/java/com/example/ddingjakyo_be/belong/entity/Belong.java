33be0fca38896bfb44c081cdd81985094837b482
Gopistol <rhrjs0131@naver.com>
2023-11-07 23:35:30 +0900

    feat: Belong 사이테이블 엔티티 생성

---
+++ src/main/java/com/example/ddingjakyo_be/belong/entity/Belong.java
@@ -0,0 +1,30 @@
+package com.example.ddingjakyo_be.belong.entity;
+
+import com.example.ddingjakyo_be.member.domain.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Belong {
+
+  @Id
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "MEMBER_ID")
+  private Member member;
+
+  @ManyToOne
+  @JoinColumn(name = "TEAM_ID")
+  private Team team;
+}
---
+++ src/main/java/com/example/ddingjakyo_be/belong/repository/Belong.java
@@ -0,0 +1,7 @@
+package com.example.ddingjakyo_be.belong.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Belong extends JpaRepository<Belong, Long> {
+
+}
