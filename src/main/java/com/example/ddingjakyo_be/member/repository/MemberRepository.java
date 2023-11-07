54663e5d69246537ea19d9e0375cc16957972d0e
Gopistol <rhrjs0131@naver.com>
2023-11-07 23:35:09 +0900

    feat: Member 엔티티 구현

---
+++ src/main/java/com/example/ddingjakyo_be/member/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.example.ddingjakyo_be.member.repository;
+
+import com.example.ddingjakyo_be.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
