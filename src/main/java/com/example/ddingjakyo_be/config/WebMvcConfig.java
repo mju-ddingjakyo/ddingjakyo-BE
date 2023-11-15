75c758572db5e28e69118188712b0b183b740d31
Gopistol <rhrjs0131@naver.com>
2023-11-15 15:10:05 +0900

    refactor: 인터셉터 세션 확인 로직 수정

--- src/main/java/com/example/ddingjakyo_be/config/WebMvcConfig.java
+++ src/main/java/com/example/ddingjakyo_be/config/WebMvcConfig.java
@@ -10,9 +10,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
-//    registry.addInterceptor(new LogInCheckInterceptor())
-//        .addPathPatterns("/**") // 모든 URI에서 인터셉터 실행
-//        .excludePathPatterns("/api/log*", "/api/register",
-//            "/api/teams"); // login, logout, 회원가입, 전체 팀 조회 URI는 인터셉터 실행에서 제외
+    registry.addInterceptor(new LogInCheckInterceptor())
+        .addPathPatterns("/**") // 모든 URI에서 인터셉터 실행
+        .excludePathPatterns("/api/log*", "/api/register",
+            "/api/teams"); // login, logout, 회원가입, 전체 팀 조회 URI는 인터셉터 실행에서 제외
   }
 }
