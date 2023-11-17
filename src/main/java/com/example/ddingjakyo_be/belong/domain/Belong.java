31db8a1012b690775a6962779a4cc98684052272
koseonje <kosunje1455@navet.com>
2023-11-17 21:18:57 +0900

    fix: GeneratedValue어노테이션 추가

--- src/main/java/com/example/ddingjakyo_be/belong/domain/Belong.java
+++ src/main/java/com/example/ddingjakyo_be/belong/domain/Belong.java
@@ -3,6 +3,8 @@ package com.example.ddingjakyo_be.belong.domain;
 import com.example.ddingjakyo_be.member.domain.Member;
 import com.example.ddingjakyo_be.team.domain.Team;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -18,6 +20,7 @@ import lombok.NoArgsConstructor;
 public class Belong {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   @ManyToOne
