c50d8daa74863386f4ee2d43347f450dfba12ee3
koseonje <kosunje1455@navet.com>
2023-11-24 03:31:54 +0900

    feat: S3사용해 이미지 업로드 구현

--- src/main/java/com/example/ddingjakyo_be/domain/member/service/MemberService.java
+++ src/main/java/com/example/ddingjakyo_be/domain/member/service/MemberService.java
@@ -1,26 +1,33 @@
 package com.example.ddingjakyo_be.domain.member.service;
 
+import com.example.ddingjakyo_be.aws.S3Service;
+import com.example.ddingjakyo_be.domain.member.controller.dto.request.MemberAuthRequest;
 import com.example.ddingjakyo_be.domain.member.controller.dto.request.MemberProfileRequest;
 import com.example.ddingjakyo_be.domain.member.controller.dto.response.MemberProfileResponse;
 import com.example.ddingjakyo_be.domain.member.controller.dto.response.MemberResponse;
 import com.example.ddingjakyo_be.domain.member.domain.Member;
 import com.example.ddingjakyo_be.domain.member.repository.MemberRepository;
-import com.example.ddingjakyo_be.domain.member.controller.dto.request.MemberAuthRequest;
 import jakarta.transaction.Transactional;
+import java.io.IOException;
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
+@Slf4j
 public class MemberService {
 
   private final MemberRepository memberRepository;
   private final PasswordEncoder passwordEncoder;
+  private final S3Service s3Service;
 
   public MemberResponse login(final String email, final String password) {
     Member member = memberRepository.findMemberByEmail(email)
@@ -59,29 +66,48 @@ public class MemberService {
     return MemberProfileResponse.from(member);
   }
 
-  public Set<Member> findMembersByEmails(final List<String> emails) {
-      return emails.stream()
-          .map(email -> memberRepository.findMemberByEmail(email).orElseThrow(()-> new IllegalArgumentException("이메일이 틀렸습니다")))
-          .collect(Collectors.toSet());
-  }
-
-  public Member findMemberById(Long memberId) {
-    return memberRepository.findById(memberId).orElseThrow(IllegalArgumentException::new);
-  }
-
   public void updateMemberProfile(MemberProfileRequest memberProfileRequest, Long memberId) {
-    // 세션의 memberId와 비교 후 같으면 수정
     Member member = findMemberById(memberId);
-    System.out.println("ID: " + memberId);
-    System.out.println(memberProfileRequest.getNickname());
-
-    Member updateMember = memberProfileRequest.toEntity();
+    deletePrevImage(member.getProfileImage());
+    Member updateMember = memberProfileRequest.toEntity(getUploadImageFileName(memberProfileRequest));
     member.updateMemberProfile(updateMember);
+  }
+
+  private void deletePrevImage(String profileImage) {
+    if(profileImage != null){
+      s3Service.delete(profileImage);
+    }
   }
 
   public void deleteMember(final Long memberId) {
     Member member = findMemberById(memberId);
     // 삭제하려는 클라이언트의 세션 정보의 memberId와 비교 후 같으면 삭제, 아니면 실패 응답
     memberRepository.delete(member);
+  }
+
+  public Set<Member> findMembersByEmails(final List<String> emails) {
+    return emails.stream()
+        .map(email -> memberRepository.findMemberByEmail(email).orElseThrow(()-> new IllegalArgumentException("이메일이 틀렸습니다")))
+        .collect(Collectors.toSet());
+  }
+
+  public Member findMemberById(Long memberId) {
+    return memberRepository.findById(memberId).orElseThrow(IllegalArgumentException::new);
+  }
+
+  private String getUploadImageFileName(MemberProfileRequest memberProfileRequest) {
+   String fileName = "";
+    for (MultipartFile multipartFile : memberProfileRequest.getImage()) {
+      // 파일명 지정 (겹치면 안되고, 확장자 빼먹지 않도록 조심!)
+      fileName = UUID.randomUUID() + multipartFile.getOriginalFilename();
+      // 파일데이터와 파일명 넘겨서 S3에 저장
+      try {
+        s3Service.upload(multipartFile, fileName);
+      } catch (IOException e) {
+        log.warn("이미지 업로드 실패");
+        throw new IllegalArgumentException("이미지 업로드 실패");
+      }
+    }
+    return fileName;
   }
 }
