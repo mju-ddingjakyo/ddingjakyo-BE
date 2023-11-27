279036b8d2316f18417d9f7f2ab661bddc49f035
koseonje <kosunje1455@navet.com>
2023-11-27 23:39:55 +0900

    fix: S3 이미지 url로 반환

--- src/main/java/com/example/ddingjakyo_be/aws/S3Service.java
+++ src/main/java/com/example/ddingjakyo_be/aws/S3Service.java
@@ -6,6 +6,7 @@ import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import java.io.IOException;
 import java.net.URL;
+import java.net.URLDecoder;
 import java.util.Date;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,16 +24,16 @@ public class S3Service {
   private final AmazonS3 amazonS3;
 
   /* 1. 파일 업로드 */
-  public void upload(MultipartFile multipartFile, String s3FileName) throws IOException {
+  public String upload(MultipartFile multipartFile, String s3FileName) throws IOException {
     // 메타데이터 생성
     ObjectMetadata objMeta = new ObjectMetadata();
     objMeta.setContentLength(multipartFile.getInputStream().available());
     // putObject(버킷명, 파일명, 파일데이터, 메타데이터)로 S3에 객체 등록
     amazonS3.putObject(bucket, s3FileName, multipartFile.getInputStream(), objMeta);
     // 등록된 객체의 url 반환 (decoder: url 안의 한글or특수문자 깨짐 방지)
-//    return URLDecoder.decode(amazonS3.getUrl(bucket, s3FileName).toString(), "utf-8");
+    return URLDecoder.decode(amazonS3.getUrl(bucket, s3FileName).toString(), "utf-8");
   }
-
+  
   /* 2. 파일 삭제 */
   public void delete (String keyName) {
     // deleteObject(버킷명, 키값)으로 객체 삭제
